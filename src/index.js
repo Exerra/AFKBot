507723e0d556aeee4415bee1300d73cd42dd0100
Exerra <exerra@exerra.xyz>
2023-02-26 13:45:56 +0200

    look at ppl

--- src/index.js
+++ src/index.js
@@ -3,14 +3,33 @@ require('dotenv').config()
 
 
 const { HOST, USERNAME, PASSWORD, AUTH_PROVIDER, HOST_PORT } = process.env
+const init = async () => {
 
-const bot = mineflayer.createBot({
-	host: HOST,
-	username: USERNAME,
-	password: PASSWORD,
-	auth: AUTH_PROVIDER,
-})
+	const bot = await mineflayer.createBot({
+		host: HOST,
+		username: USERNAME,
+		password: PASSWORD,
+		auth: AUTH_PROVIDER,
+		physicsEnabled: true
+	})
 
-// Log errors and kick reasons:
-bot.on('kicked', console.log)
-bot.on('error', console.log)
+	function lookAtNearestPlayer() {
+		const playerEntity = bot.nearestEntity()
+
+		if (!playerEntity) return;
+
+		const pos = playerEntity.position
+		bot.lookAt(pos)
+	}
+
+	bot.on('physicTick', () => {
+
+		lookAtNearestPlayer()
+	})
+
+	// Log errors and kick reasons:
+	bot.on('kicked', console.log)
+	bot.on('error', console.log)
+}
+
+init()
